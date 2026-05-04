d1438073ec58c4c3dd3c0e92263ecf790db40065
Rodrigo <rodrigozdurigan@gmail.com>
2026-05-04 15:33:42 -0300

    Iniciando projeto SpringBoot

---
+++ liquid-democracy/src/main/java/com/rzanetti/liquid/democracy/LiquidDemocracyApplication.java
@@ -0,0 +1,13 @@
+package com.rzanetti.liquid.democracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidDemocracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidDemocracyApplication.class, args);
+	}
+
+}
---
+++ liquid-democracy/src/test/java/com/rzanetti/liquid/democracy/LiquidDemocracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzanetti.liquid.democracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidDemocracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
